67fe83763fbf5c848ac106df5e5dd0fbfe71be42
Martyn Clinton <ausipome@gmail.com>
2024-09-24 19:58:57 +0100

    up

--- components/user/IsLoggedIn.tsx
+++ components/user/IsLoggedIn.tsx
@@ -3,13 +3,14 @@
 import { useAuth } from '../../hooks/useAuth'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { byPrefixAndName } from '@awesome.me/kit-515ba5c52c/icons';
+import Link from 'next/link';
 
 export default function IsLoggedIn() {
     const { isAuthenticated } = useAuth();
 
     return (
         <>
-            {isAuthenticated ? <FontAwesomeIcon icon={byPrefixAndName.fal['eye-slash']} /> : <FontAwesomeIcon icon={byPrefixAndName.fal['eye']} />}
+            {isAuthenticated ? <><Link className="text-white py-2 hover:text-gray-700 px-4 text-3xl mr-4" href="account"><FontAwesomeIcon icon={byPrefixAndName.fal['user']} /></Link></> : <><Link className="text-white py-2 hover:text-gray-700 px-4 text-xl" href="signup">Signup</Link><Link className="text-white py-2 hover:text-gray-700 px-4 text-xl" href="login">Login</Link></>}
         </>
     );
 }
